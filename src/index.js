eb3b3763c7688e91e68acb709ec672980b31749f
illicchpv <illich58@mail.ru>
2023-03-17 16:13:27 +0300

    correction

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import "./styles/main.scss"
-import "./images/arrow.svg"
-import "./images/banner.jpg"
-import "./images/Vebinar1_2.png"
+import "./styles/main.scss";
+import "./images/arrow.svg";
+import "./images/banner.jpg";
+import "./images/Vebinar1_2.png";
