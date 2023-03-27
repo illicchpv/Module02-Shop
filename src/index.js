75e77e518f3008ea58a9feec95578a5ebd5a63a9
illicchpv <illich58@mail.ru>
2023-03-27 11:46:16 +0300

    end of lesson 11

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 import "normalize.css";
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import "./styles/root.scss";
+
 import "./index.html";
+
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+// // core version + navigation, pagination modules:
+// import Swiper, { Navigation, Pagination } from 'swiper';
+// // import Swiper and modules styles
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
+
+// // init Swiper:
+// const swiper = new Swiper('.swiper', {
+//   // configure Swiper to use modules
+//   modules: [Navigation, Pagination],
+//   ...
+// });
+
+var swiper = new Swiper(".offerSwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  modules: [Navigation, Pagination],
+});
